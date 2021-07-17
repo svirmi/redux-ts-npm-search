1481a1b81cfddb42f97f55686dfd3c6c46c1ceda
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2021-07-17 16:15:16 +0300

    split Action interface to three separate interfaces

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -4,23 +4,38 @@ interface RepositoriesState {
     data: string[];
 }
 
-interface Action {
-    type: string;
-    payload?: any
+interface SearchRepositoriesAction {
+    type: 'search_repositories';
 }
 
-const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
-    switch (action.type) {
+interface SearchRepositoriesSuccessAction {
+    type: 'search_repositories_success';
+    payload: string[];
+}
 
-        case 'search_repositories':
-            return { loading: true, error: null, data: [] };
-        case 'search_repositories_success':
-            return { loading: false, error: null, data: action.payload };
-        case 'search_repositories_error':    
-            return { loading: false, error: action.payload, data: [] };
-        default:
-            return state;
-    }
+interface SearchRepositoriesErrorAction {
+    type: 'search_repositories_error';
+    payload: string;
+}
+
+const reducer = (
+    state: RepositoriesState, 
+    action: 
+        SearchRepositoriesAction | 
+        SearchRepositoriesSuccessAction | 
+        SearchRepositoriesErrorAction 
+    ): RepositoriesState => {
+
+        switch (action.type) {
+            case 'search_repositories':
+                return { loading: true, error: null, data: [] };
+            case 'search_repositories_success':
+                return { loading: false, error: null, data: action.payload };
+            case 'search_repositories_error':    
+                return { loading: false, error: action.payload, data: [] };
+            default:
+                return state;
+        }
 }
 
 export default reducer;
