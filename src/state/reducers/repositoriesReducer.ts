36e1300baba742da1d2d53adb463057e45d22d3a
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2021-07-17 19:59:44 +0300

    refactored: split code to separate files

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -1,30 +1,11 @@
-enum ActionType {
-    SEARCH_REPOSITORIES = 'search_repositories',
-    SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
-    SEARCH_REPOSITORIES_ERROR = 'search_repositories_error',
-}
+import { ActionType } from '../action-types'
+import { Action } from '../actions'
 
 interface RepositoriesState {
     loading: boolean;
     error: string | null;
     data: string[];
 }
-
-interface SearchRepositoriesAction {
-    type: ActionType.SEARCH_REPOSITORIES;
-}
-
-interface SearchRepositoriesSuccessAction {
-    type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
-    payload: string[];
-}
-
-interface SearchRepositoriesErrorAction {
-    type: ActionType.SEARCH_REPOSITORIES_ERROR;
-    payload: string;
-}
-
-type Action = SearchRepositoriesAction | SearchRepositoriesSuccessAction | SearchRepositoriesErrorAction 
 
 const reducer = (
     state: RepositoriesState, 
